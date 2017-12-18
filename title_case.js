3edb229b018216a084219cf81dd0df89d97564ab
Rebelpebble <neasbrian@gmail.com>
2017-12-17 20:32:23 -0600

    Clean up

--- title_case.js
+++ title_case.js
@@ -1,6 +1,5 @@
 function convertToTitleCase(words) {
   const allWords = words.split(' ')
-
   const allWordsCapitalized = allWords.map(word => word.charAt(0).toUpperCase() + word.slice(1))
 
   console.log(allWordsCapitalized.join(' '))
