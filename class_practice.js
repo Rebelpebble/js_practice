3c936fa1ba4373c6bcd75df8b3fd3d622fc6ea52
Rebelpebble <neasbrian@gmail.com>
2017-12-18 22:07:17 -0600

    Create class practice program

---
+++ class_practice.js
@@ -0,0 +1,33 @@
+class Weapons {
+  constructor(model, ammo, capacity) {
+    this._model = model
+    this._ammo = ammo
+    this._capacity = capacity
+  }
+
+  get capacityOfWeapon() {
+    return this._capacity
+  }
+
+  printWeaponInformation() {
+    console.log("This is a:" + this._model + "that uses" + this._ammo)
+    }
+
+  static generateSerialNumber() {
+    this._serialNumber = Math.floor(Math.random() * 10000)
+  }
+}
+
+class AssultRifle extends Weapons {
+  constructor(name, ammo, capacity) {
+    super(name, ammo, capacity)
+    this._firingStyle = 'semi-auto'
+  }
+
+  get firingStyle() {
+    return this._firingStyle
+  }
+}
+
+const briansGun = new AssultRifle('AK47', 7.62, 50)
+briansGun.generateSerialNumber()
