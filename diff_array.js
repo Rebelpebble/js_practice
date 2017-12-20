fb11dd575f7200fabd4ef8940c2315666ed038eb
Rebelpebble <neasbrian@gmail.com>
2017-12-19 23:19:09 -0600

    Create function to reduce repeating forEach methods

--- diff_array.js
+++ diff_array.js
@@ -1,30 +1,18 @@
 function diffArray(arr1, arr2) {
-  arraysOrdered = maxArrayFirst(arr1, arr2)
-  maxArray = arraysOrdered[0]
-  minArray = arraysOrdered[1]
-  arrayUncommon = []
+  const arrayUncommon = []
 
-  maxArray.forEach(function(element) {
-    if (minArray.indexOf(element) === -1) {
-      arrayUncommon.push(element)
-    }
-  })
-
-  minArray.forEach(function(element) {
-    if (maxArray.indexOf(element) === -1) {
-      arrayUncommon.push(element)
-    }
-  })
+  pushAllUncommonElements(arr1, arr2, arrayUncommon)
+  pushAllUncommonElements(arr2, arr1, arrayUncommon)
 
   console.log(arrayUncommon)
 }
 
-function maxArrayFirst(firstArray, secondArray) {
-  if (firstArray.length >= secondArray.length) {
-    return [firstArray, secondArray]
-  } else {
-    return [secondArray, firstArray]
-  }
+function pushAllUncommonElements(firstArray, secondArray, uncommons) {
+  firstArray.forEach(function(element) {
+    if (secondArray.indexOf(element) === -1) {
+      uncommons.push(element)
+    }
+  })
 }
 
-diffArray([1,2,3,4,16], [1,2,3,4,5,7,9])
+diffArray([1,2,3,4,16,3,56,43,57], [1,2,3,4,5,7,9])
